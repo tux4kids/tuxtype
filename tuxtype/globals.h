7ecd0b5956d7712d945373f8798f1999c8e8a036
Mobin Mohan <mobstux @nospam@ gmail.com>
2007-08-21 18:11:45 +0000

    Changed the malayalam ttf font in the indic subproject
    git-svn-id: svn://svn.debian.org/svn/tux4kids/tuxtype/branches/tuxtype-indic@210 47d30e19-490b-0410-9d18-e851f4e465b3

--- tuxtype/globals.h
+++ tuxtype/globals.h
@@ -110,10 +110,10 @@ typedef struct {
 #define amask 0xff000000
 #endif
 
-#define menu_font   "Rachana_g01.ttf" /* "AndikaDesRevG.ttf"    "AndikaDesRevA.ttf"  GenAI102.ttf or "DejaVuSans-Bold.ttf"  or "FreeSansBold.ttf" */ 	/* was menu.ttf */
+#define menu_font   "Rachana_w01.ttf" /* "Rachana_g01.ttf"  "AndikaDesRevG.ttf"    "AndikaDesRevA.ttf"  GenAI102.ttf or "DejaVuSans-Bold.ttf"  or "FreeSansBold.ttf" */ 	/* was menu.ttf */
 #define menu_font_size	20
 
-#define ttf_font    "Rachana_g01.ttf" /* "AndikaDesRevG.ttf" AndikaDesRevA.ttf"  GenAI102.ttf or "DejaVuSans-Bold.ttf" or "FreeSansBold.ttf" */  	/* was letters.ttf */
+#define ttf_font   "Rachana_w01.ttf" /* "Rachana_g01.ttf"  "AndikaDesRevG.ttf" AndikaDesRevA.ttf"  GenAI102.ttf or "DejaVuSans-Bold.ttf" or "FreeSansBold.ttf" */  	/* was letters.ttf */
 #define ttf_font_size	20
 
 /* Limits on word list size, word length, and on the number of distinct characters */
