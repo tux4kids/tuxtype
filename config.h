7f09069f79cd68326863ea9baaf26e1d5c320bd5
David Bruce <davidstuartbruce @nospam@ gmail.com>
2007-11-29 10:42:22 +0000

    Added Czech theme from Jaroslav Krejčí (JardaK) - <krejci@zstenis.com>
    git-svn-id: svn://svn.debian.org/svn/tux4kids/tuxtype/trunk@347 47d30e19-490b-0410-9d18-e851f4e465b3

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 #define BUILD_MINGW32 1
 
 /* Define to 1 if you have the `atexit' function. */
-/* #undef HAVE_ATEXIT */
+#define HAVE_ATEXIT 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
@@ -17,19 +17,19 @@
 #define HAVE_LIBSDL_MIXER 1
 
 /* Define to 1 if you have the `SDL_Pango' library (-lSDL_Pango). */
-#define HAVE_LIBSDL_PANGO 1
+/* #undef HAVE_LIBSDL_PANGO */
 
 /* Define to 1 if you have the `SDL_ttf' library (-lSDL_ttf). */
 #define HAVE_LIBSDL_TTF 1
 
 /* Define to 1 if you have the `malloc' function. */
-/* #undef HAVE_MALLOC */
+#define HAVE_MALLOC 1
 
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the `memset' function. */
-/* #undef HAVE_MEMSET */
+#define HAVE_MEMSET 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
@@ -38,7 +38,7 @@
 #define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the `strchr' function. */
-/* #undef HAVE_STRCHR */
+#define HAVE_STRCHR 1
 
 /* Define to 1 if you have the <strings.h> header file. */
 #define HAVE_STRINGS_H 1
@@ -47,7 +47,7 @@
 #define HAVE_STRING_H 1
 
 /* Define to 1 if you have the `strstr' function. */
-/* #undef HAVE_STRSTR */
+#define HAVE_STRSTR 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
@@ -62,25 +62,25 @@
 #define PACKAGE "tuxtype"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "tuxmath-devel@lists.sourceforge.net"
+#define PACKAGE_BUGREPORT "tux4kids-tuxtype-dev@lists.alioth.debian.org"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "tuxtype"
+#define PACKAGE_NAME "Tux Typing"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "tuxtype 1.5.13"
+#define PACKAGE_STRING "Tux Typing 1.5.16"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "tuxtype"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.5.13"
+#define PACKAGE_VERSION "1.5.16"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.5.13"
+#define VERSION "1.5.16"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
