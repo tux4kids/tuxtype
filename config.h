8123e1ad57e2174d232defbe5af2281b2fa5a460
Michael Hsieh <michaelhsieh.alt @nospam@ gmail.com>
2008-08-31 07:34:22 +0000

    missing config files
    git-svn-id: svn://svn.debian.org/svn/tux4kids/tuxtype/branches/tuxtype-soc-mh@723 47d30e19-490b-0410-9d18-e851f4e465b3

---
+++ config.h
@@ -0,0 +1,92 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Native MinGW32 build */
+/* #undef BUILD_MINGW32 */
+
+/* Define to 1 if you have the `atexit' function. */
+#define HAVE_ATEXIT 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `SDL_image' library (-lSDL_image). */
+#define HAVE_LIBSDL_IMAGE 1
+
+/* Define to 1 if you have the `SDL_mixer' library (-lSDL_mixer). */
+#define HAVE_LIBSDL_MIXER 1
+
+/* Define to 1 if you have the `SDL_Pango' library (-lSDL_Pango). */
+#define HAVE_LIBSDL_PANGO 1
+
+/* Define to 1 if you have the `SDL_ttf' library (-lSDL_ttf). */
+#define HAVE_LIBSDL_TTF 1
+
+/* Define to 1 if you have the `malloc' function. */
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strchr' function. */
+#define HAVE_STRCHR 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strstr' function. */
+#define HAVE_STRSTR 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Name of package */
+#define PACKAGE "tuxtype"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "tux4kids-tuxtype-dev@lists.alioth.debian.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Tux Typing"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Tux Typing 1.5.16"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "tuxtype"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.5.16"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.5.16"
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
