fa3e2d7412fe0ea8c94216d75bd074e6980348ee
David Bruce <davidstuartbruce @nospam@ gmail.com>
2009-01-14 16:44:47 +0000

    Added remainder of fonts needed for rest of Indic languages
    git-svn-id: svn://svn.debian.org/svn/tux4kids/tuxtype/trunk@845 47d30e19-490b-0410-9d18-e851f4e465b3

--- src/loaders.c
+++ src/loaders.c
@@ -73,24 +73,24 @@ void LoadLang(void)
   char *s1, *s2, *s3, *s4;
   char buf [30];
   s1 = setlocale(LC_ALL, settings.theme_locale_name);
-//  s2 = bindtextdomain(PACKAGE, TUXLOCALE);
-//  s3 = bind_textdomain_codeset(PACKAGE, "UTF-8");
-//  s4 = textdomain(PACKAGE);
+  s2 = bindtextdomain(PACKAGE, TUXLOCALE);
+  s3 = bind_textdomain_codeset(PACKAGE, "UTF-8");
+  s4 = textdomain(PACKAGE);
 
   DEBUGCODE
   {
-//    fprintf(stderr, "PACKAGE = %s\n", PACKAGE);
-//    fprintf(stderr, "TUXLOCALE = %s\n", TUXLOCALE);
+    fprintf(stderr, "PACKAGE = %s\n", PACKAGE);
+    fprintf(stderr, "TUXLOCALE = %s\n", TUXLOCALE);
     fprintf(stderr, "setlocale(LC_ALL, %s) returned: %s\n", settings.theme_locale_name, s1);
-//    fprintf(stderr, "bindtextdomain(PACKAGE, TUXLOCALE) returned: %s\n", s2);
-//    fprintf(stderr, "bind_textdomain_codeset(PACKAGE, \"UTF-8\") returned: %s\n", s3);
-//    fprintf(stderr, "textdomain(PACKAGE) returned: %s\n", s4);
+    fprintf(stderr, "bindtextdomain(PACKAGE, TUXLOCALE) returned: %s\n", s2);
+    fprintf(stderr, "bind_textdomain_codeset(PACKAGE, \"UTF-8\") returned: %s\n", s3);
+    fprintf(stderr, "textdomain(PACKAGE) returned: %s\n", s4);
     fprintf(stderr, "gettext(\"Fish\"): %s\n\n", gettext("Fish"));
 //    fprintf(stderr, "After gettext() call\n");
   }
 
-  /* Also set LANG as fallback because setlocale() unreliable */
-  /* on some Windows versions, AFAICT                         */
+  /* Also set LANG and LANGUAGE as fallbacks because setlocale() unreliable */
+  /* on some Windows versions, AFAICT                                       */
   snprintf(buf, 30, "%s", settings.theme_locale_name);
   buf[5] = '\0';  //en_US" rather than "en_US.utf8"
   DEBUGCODE { fprintf(stderr, "buf is: %s\n", buf); }
@@ -98,6 +98,11 @@ void LoadLang(void)
   if (my_setenv("LANG", buf) == -1)
   {
     fprintf(stderr, "Warning - could not set LANG to %s\n\n", buf);
+  }
+
+  if (my_setenv("LANGUAGE", buf) == -1)
+  {
+    fprintf(stderr, "Warning - could not set LANGUAGE to %s\n\n", buf);
   }
 
   return;
