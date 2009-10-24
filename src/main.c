83a9c261a8977b4a91b5f1f32807ae99037643f3
David Bruce <davidstuartbruce @nospam@ gmail.com>
2009-10-24 14:38:53 +0000

    updated strings for version message
    git-svn-id: svn://svn.debian.org/svn/tux4kids/tuxtype/trunk@1603 47d30e19-490b-0410-9d18-e851f4e465b3

--- src/main.c
+++ src/main.c
@@ -79,8 +79,10 @@ int main(int argc, char *argv[])
          || (strcmp(argv[i], "--version") == 0))
       {
         fprintf(stderr, "\n%s, Version %s\n", PACKAGE, VERSION);
-        fprintf(stderr, "Copyright (C) Sam Hart <hart@geekcomix.com>, under the GPL\n");
-        fprintf(stderr, "-See COPYING file for more info... Thx ;)\n\n");
+        fprintf(stderr, "Copyright 2000-2009 Sam Hart, Jesse Andrews, David Bruce, and the Tux4Kids team.\n");
+        fprintf(stderr, "Tux4Kids website: http://tux4kids.alioth.debian.org\n");
+        fprintf(stderr, "This software is licensed under the GNU General Public License, Version 2 or later, and compatible media licenses.\n");
+        fprintf(stderr, "See COPYING file for licensing details.\n\n");
         exit(0);
       }
 
@@ -134,7 +136,7 @@ int main(int argc, char *argv[])
     s3 = bind_textdomain_codeset(PACKAGE, "UTF-8");
     s4 = textdomain(PACKAGE);
 
-//    DEBUGCODE
+    DEBUGCODE
     {
       fprintf(stderr, "PACKAGE = %s\n", PACKAGE);
       fprintf(stderr, "TUXLOCALE = %s\n", TUXLOCALE);
