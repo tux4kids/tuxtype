fc6a7fc21e5ee14b2a43fb59fbd338ee98ff649a
David Bruce <davidstuartbruce @nospam@ gmail.com>
2009-12-23 03:30:52 +0000

    more work on input methods code from Tux Paint
    git-svn-id: svn://svn.debian.org/svn/tux4kids/tuxtype/trunk@1694 47d30e19-490b-0410-9d18-e851f4e465b3

--- src/input_methods.c
+++ src/input_methods.c
@@ -219,6 +219,98 @@ static int im_initialized = 0;
 */
 static IM_EVENT_FN im_event_fp = NULL;
 
+/* Language enum copied from Tux Paint's i18n.h (same authors) to */
+/* avoid need to separately include that file:                    */
+/* Possible languages: */
+
+enum
+{
+  LANG_AF,			/* Afrikaans */
+  LANG_AR,			/* Arabic */
+  LANG_AST,			/* Asturian */
+  LANG_AZ,			/* Azerbaijani */
+  LANG_BE,			/* Belarusian */
+  LANG_BG,			/* Bulgarian */
+  LANG_BO,			/* Tibetan */
+  LANG_BR,			/* Breton */
+  LANG_CA,			/* Catalan */
+  LANG_CS,			/* Czech */
+  LANG_CY,			/* Welsh */
+  LANG_DA,			/* Danish */
+  LANG_DE,			/* German */
+  LANG_EL,			/* Greek */
+  LANG_EN,			/* English (American) (DEFAULT) */
+  LANG_EN_AU,			/* English (Australian) */
+  LANG_EN_CA,			/* English (Canadian) */
+  LANG_EN_GB,			/* English (British) */
+  LANG_EN_ZA,			/* English (South African) */
+  LANG_EO,			/* Esperanto */
+  LANG_ES,			/* Spanish */
+  LANG_ES_MX,			/* Spanish (Mexican) */
+  LANG_ET,			/* Estonian */
+  LANG_EU,			/* Basque */
+  LANG_FI,			/* Finnish */
+  LANG_FO,			/* Faroese */
+  LANG_FR,			/* French */
+  LANG_GA,			/* Irish Gaelic */
+  LANG_GD,			/* Scottish Gaelic */
+  LANG_GL,			/* Galician */
+  LANG_GR,			/* Gronings */
+  LANG_GU,			/* Gujarati */
+  LANG_HE,			/* Hebrew */
+  LANG_HI,			/* Hindi */
+  LANG_HR,			/* Croatian */
+  LANG_HU,			/* Hungarian */
+  LANG_I_KLINGON_ROMANIZED,	/* Klingon (Romanized) */
+  LANG_ID,			/* Indonesian */
+  LANG_IS,			/* Icelandic */
+  LANG_IT,			/* Italian */
+  LANG_JA,			/* Japanese */
+  LANG_KA,			/* Georgian */
+  LANG_KM,			/* Khmer */
+  LANG_KO,			/* Korean */
+  LANG_KU,			/* Kurdish */
+  LANG_LT,			/* Lithuanian */
+  LANG_LV,			/* Latvian */
+  LANG_MK,			/* Macedonian */
+  LANG_MS,			/* Malay */
+  LANG_NB,			/* Norwegian Bokmal */
+  LANG_NL,			/* Dutch */
+  LANG_NN,			/* Norwegian Nynorsk */
+  LANG_NR,                      /* Ndebele */
+  LANG_OC,			/* Occitan */
+  LANG_OJ,			/* Ojibway */
+  LANG_PL,			/* Polish */
+  LANG_PT_BR,			/* Portuguese (Brazilian) */
+  LANG_PT_PT,			/* Portuguese (Portugal) */
+  LANG_RO,			/* Romanian */
+  LANG_RU,			/* Russian */
+  LANG_RW,			/* Kinyarwanda */
+  LANG_SHS,			/* Shuswap */
+  LANG_SK,			/* Slovak */
+  LANG_SL,			/* Slovenian */
+  LANG_SON,                     /* Songhay */
+  LANG_SQ,			/* Albanian */
+  LANG_SR,			/* Serbian */
+  LANG_SV,			/* Swedish */
+  LANG_SW,			/* Swahili */
+  LANG_TA,			/* Tamil */
+  LANG_TE,                      /* Telugu */
+  LANG_TH,			/* Thai */
+  LANG_TL,			/* Tagalog */
+  LANG_TR,			/* Turkish */
+  LANG_TWI,			/* Twi */
+  LANG_UK,			/* Ukrainian */
+  LANG_VE,			/* Venda */
+  LANG_VI,			/* Vietnamese */
+  LANG_WA,			/* Walloon */
+  LANF_WO,                      /* Wolof */
+  LANG_XH,			/* Xhosa */
+  LANG_ZH_CN,			/* Chinese (Simplified) */
+  LANG_ZH_TW,			/* Chinese (Traditional) */
+  LANG_ZAM,			/* Zapotec (Miahuatlan) */
+  NUM_LANGS
+};
 
 /** Moved from Tux Paint's i18n.c: */
 const char *lang_prefixes[NUM_LANGS] = {
@@ -317,7 +409,7 @@ const char *lang_prefixes[NUM_LANGS] = {
 * @see im_init()
 * @see im_read()
 */
-static IM_EVENT_FN im_event_fns[NUM_LANGS];
+static IM_EVENT_FN im_event_fns[NUM_LANGS] = {NULL};
 
 
 /* ***************************************************************************
@@ -1699,7 +1791,9 @@ void im_init(IM_DATA* im, int lang)
 
   #ifdef DEBUG
   assert(0 <= im->lang && im->lang < NUM_LANGS);
-  if(im_event_fp) printf("Initializing IM for %s...\n", lang_prefixes[im->lang]);
+  
+  if(im_event_fns[im->lang])
+    printf("Initializing IM for %s...\n", lang_prefixes[im->lang]);
   #endif
 
   /* Initialize the individual IM */
@@ -1752,7 +1846,7 @@ void im_free(IM_DATA* im)
 */
 int im_read(IM_DATA* im, SDL_keysym ks)
 {
-  im_event_fp = NULL;
+//  im_event_fp = NULL;
   int redraw = 0;
 
   /* Sanity check */
@@ -1765,8 +1859,10 @@ int im_read(IM_DATA* im, SDL_keysym ks)
   im_event_fp = im_event_fns[im->lang];
 
   /* Run the language-specific IM or run the default C IM */
-  if(im_event_fp) redraw = (*im_event_fp)(im, ks);
-  else redraw = im_event_c(im, ks);
+  if(im_event_fp)
+    redraw = (*im_event_fp)(im, ks);
+  else
+    redraw = im_event_c(im, ks);
 
   #ifdef IM_DEBUG
   wprintf(L"* [%8ls] [%8ls] %2d %2d (%2d)\n", im->s, im->buf, wcslen(im->s), wcslen(im->buf), im->redraw);
