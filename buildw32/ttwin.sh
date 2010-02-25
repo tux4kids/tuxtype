dd6ff95591fcd374742409b102279fae495b3fb8
David Bruce <davidstuartbruce@gmail.com>
2010-02-24 20:19:30 -0600

    Support for win32 crossbuild with mingw-cross-env

--- buildw32/ttwin.sh
+++ buildw32/ttwin.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
 # An 'all-in-one' script to build the Windows installer executable.
+# NOTE we have to use sudo because "make install" has a chown root command
 
-./cross-make.sh clean
-./cross-make.sh distclean
+echo "Cleaning build directory:"
+sudo ./cross-make.sh clean>/dev/null
+sudo ./cross-make.sh distclean>/dev/null
+echo "Running autoreconf --install .."
 autoreconf --install ..
-./cross-configure.sh --without-sdlpango --without-rsvg
+echo "Running ./cross-configure.sh --host=i686-pc-mingw32"
+./cross-configure.sh --host=i686-pc-mingw32
+echo "Running ./cross-make.sh"
 ./cross-make.sh
-./cross-make.sh nsis
+echo "Running ./cross-make.sh dist (to generate gmo files)"
+./cross-make.sh dist
+echo "Building NSIS installer file"
+sudo ./cross-make.sh nsis
 
